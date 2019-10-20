476d2f70d07d87abf575ca9c8d4a67579dbc156e
Huda Yousif <hjdoski@gmail.com>
2019-10-20 08:32:39 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -74,7 +74,7 @@ class Owner
   end 
   
   def list_pets 
-    return "I have #{dog.count} dog(s), and #{cat.count} cat(s)."
+    return "I have #{dogs.count} dog(s), and #{cats.count} cat(s)."
   end 
   
 end
